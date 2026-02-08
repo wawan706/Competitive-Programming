7554e522bb7dcd00ea48285c5620f99489d58d7a
Muhammad Setiawan Pratama <138946507+wawan706@users.noreply.github.com>
2026-02-08 16:02:26 +0800

    Add function to sum adjacent elements in vector

---
+++ tlx.toki/course/basic_c++/chapter8OtherDataType/5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> jumlah_bersebelahan(vector<int> v) {
+    int n = v.size();
+    for (int i = 0; i < n - 1; i++){
+        v[i] = v[i] + v[i + 1];
+    }
+    v.pop_back();
+    return v;
+}
+
+// Fungsi cetak_vector() dan main() di bawah tidak boleh diubah!
+
+// cout tidak bisa mencetak sebuah vector, sehingga kita perlu
+// membuat fungsi tersendiri untuk mencetak vector secara manual.
+void cetak_vector(vector<int> v) {
+    cout << "vector:";
+    for (int x : v) {
+        cout << " " << x;
+    }
+    cout << endl;
+}
+
+int main() {
+    cetak_vector(jumlah_bersebelahan({1, 2, 3, 4, 5}));
+    cetak_vector(jumlah_bersebelahan({17, 8, 1945}));
+}
