83eaf26e31657fcbf0beff6937d3bdacfd6c2d65
Muhammad Setiawan Pratama <138946507+wawan706@users.noreply.github.com>
2026-02-11 22:37:13 +0800

    Implement factorial function using iteration

--- tlx.toki/course/basic_c++/chapter9recurtion/1.cpp
+++ tlx.toki/course/basic_c++/chapter9recurtion/1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int faktorial(int n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        int hasil = 1;
+        for (int i = 2; i < n; i++){
+            hasil *= i;
+        }
+        return n * hasil;
+    }
+}
+
+int main() {
+    cout << faktorial(12) << endl;
+}
