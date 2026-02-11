431405ae9c1dc43be98b0c4deeaa8b8c51a742f8
Muhammad Setiawan Pratama <138946507+wawan706@users.noreply.github.com>
2026-02-11 22:40:33 +0800

    Refactor factorial function to use recursion

--- tlx.toki/course/basic_c++/chapter9recurtion/1.cpp
+++ tlx.toki/course/basic_c++/chapter9recurtion/1.cpp
@@ -5,11 +5,7 @@ int faktorial(int n) {
     if (n == 1) {
         return 1;
     } else {
-        int hasil = 1;
-        for (int i = 2; i < n; i++){
-            hasil *= i;
-        }
-        return n * hasil;
+        return n * faktorial(n - 1);
     }
 }
 
