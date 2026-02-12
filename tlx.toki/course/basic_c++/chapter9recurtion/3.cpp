3f5525c17dd859be5da600dc31c8c0a827ddc6b7
Muhammad Setiawan Pratama <138946507+wawan706@users.noreply.github.com>
2026-02-12 16:50:31 +0800

    Add recursive function to print numbers in descending order

--- tlx.toki/course/basic_c++/chapter9recurtion/3.cpp
+++ tlx.toki/course/basic_c++/chapter9recurtion/3.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void cetak_menurun(int n) {
+    cout << n << endl;
+    
+    if (n == 2){
+        cout << 1 << endl; 
+    } else {
+        cetak_menurun(n - 1);
+    }
+    
+}
+
+// Fungsi main() di bawah tidak boleh diubah!
+int main() {
+    cetak_menurun(10);
+}
 
